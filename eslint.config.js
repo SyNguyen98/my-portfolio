9768ccc6a8d46643ec96a853486776fce6d55f7a
synguyen <nghsynguyen98@gmail.com>
2025-03-13 01:14:17 +0700

    setup unit test

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,35 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jestDom from "eslint-plugin-jest-dom";
+import testingLibrary from "eslint-plugin-testing-library";
+import vitest from "eslint-plugin-vitest";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    {ignores: ['dist']},
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: { ...globals.browser, ...vitest.environments.env.globals },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            "jest-dom": jestDom,
+            "testing-library": testingLibrary,
+            vitest,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+            ...jestDom.configs.recommended.rules,
+            ...testingLibrary.configs.react.rules,
+            ...vitest.configs.recommended.rules,
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,4 +12,17 @@ export default defineConfig({
         react(),
         tailwindcss()
     ],
+    test: {
+        globals: true,
+        environment: "jsdom",
+        // this points to the setup file created earlier
+        setupFiles: "./src/setupTests.js",
+        // you might want to disable the `css: true` line, since we don't have
+        // tests that rely on CSS -- and parsing CSS is slow.
+        // I'm leaving it in here because often people want to parse CSS in tests.
+        css: true,
+        coverage: {
+            reporter: ["text", "json", "html"], // Generates reports in different formats
+        },
+    },
 })
