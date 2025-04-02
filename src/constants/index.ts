aafc98de1901fa0bc1d57734d430311723e4df64
synguyen <nghsynguyen98@gmail.com>
2025-04-03 01:22:57 +0700

    Update links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/synguyen98/";
 export const GITHUB_URL = "https://github.com/SyNguyen98";
-export const INSTAGRAM_URL = "https://www.instagram.com/synguyen98/";
-export const FACEBOOK_URL = "https://www.facebook.com/synguyen98";
+export const INSTAGRAM_URL = "https://www.instagram.com/synguyen998/";
+export const FACEBOOK_URL = "https://www.facebook.com/nguyen.charlton";
 export const YOUTUBE_URL = "https://www.youtube.com/@nguyenandbuddies";
