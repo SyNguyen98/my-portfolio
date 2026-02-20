308f3bc647c40fdf4f3430648fe7a18e9dd83c40
synguyen <nghsynguyen98@gmail.com>
2026-02-21 03:36:21 +0700

    Update footer last update date to Feb 21st, 2026, rename setupTests.js to setupTests.ts, and adjust corresponding paths in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         globals: true,
         environment: "jsdom",
         // this points to the setup file created earlier
-        setupFiles: "./src/setupTests.js",
+        setupFiles: "./src/setupTests.ts",
         // you might want to disable the `css: true` line, since we don't have
         // tests that rely on CSS -- and parsing CSS is slow.
         // I'm leaving it in here because often people want to parse CSS in tests.
